b51849d97ad08dfe04e558964a2ec7add5368d3e
Youssef <youssefcg1@gmail.com>
2024-06-13 17:16:25 -0400

    pag principal mostly

---
+++ app/crear_post/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+import {useState} from 'react';
+import {useSession} from 'next-auth/react';
+import {useRouter} from 'next/navigation';
+
+const CrearPost = () => {
+  return (
+    <div>CrearPost</div>
+  )
+}
+
+export default CrearPost
