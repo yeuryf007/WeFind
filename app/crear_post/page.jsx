ce60b2c59830cb5603ba7206658fa607d59f676c
Youssef <youssefcg1@gmail.com>
2024-06-14 17:01:48 -0400

    Pagina de agregar productos (textboxes de nombre, precio y descripcion creadas)

--- app/crear_post/page.jsx
+++ app/crear_post/page.jsx
@@ -3,9 +3,28 @@ import {useState} from 'react';
 import {useSession} from 'next-auth/react';
 import {useRouter} from 'next/navigation';
 
+import Form from '@components/Form';
+import { traceGlobals } from 'next/dist/trace/shared';
+
 const CrearPost = () => {
+  const [submitting, setsubmitting] = useState(false);
+  const [post, setpost] = useState({
+    prompt: '',
+    tag: ''
+  })
+
+  const crearpost = async (e) => {
+
+  }
   return (
-    <div>CrearPost</div>
+    <Form 
+    type="Añadir"
+    post={post}
+    setpost = {setpost}
+    submitting = {submitting} 
+    handleSubmit = {crearpost}
+
+    />
   )
 }
 
