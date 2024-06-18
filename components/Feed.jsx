a85da96e28548e206f2075cc4167924df00287cd
Youssef <youssefcg1@gmail.com>
2024-06-18 17:13:40 -0400

    First draft de barra de busqueda

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+'use client';
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
 const Feed = () => {
+
+  const [searchText, setsearchText] = useState('');
+  const handleSearchChange = (e) => {
+    
+  }
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+        type="text"
+        placeholder="Busca un producto que desees"
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        className="search_input peer"/>
+      </form>
+    </section>
   )
 }
 
