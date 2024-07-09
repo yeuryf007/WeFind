62d99a0ff9052e5a61d05aa645a411b5b8fcdd06
yeuryf007 <yeuryf007@hotmail.com>
2024-07-08 20:18:45 -0400

    Inicializado firestore

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -8,8 +8,9 @@ const firebaseConfig = {
   storageBucket: proccess.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: proccess.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: proccess.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: proccess.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
