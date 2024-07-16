d2595f8583d3133573aaee58f017e92c8f3d5f61
yeuryf007 <yeuryf007@hotmail.com>
2024-07-16 15:22:13 -0400

    categoria casi terminada

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -3,14 +3,16 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: proccess.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: proccess.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: proccess.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: proccess.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: proccess.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: proccess.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+export { db };
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,20 +1,29 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
-const Card = () => {
+const Card = ({ product, distance }) => {
   return (
-    <div className="bg-white rounded-lg w-80 h-48 flex flex-row mt-2 p-4">
-      <Image src="/assets/images/placeholder.png" width={120} height={30} className="object-contain border-2"/>
-      <div className="w-1/2 flex-col ml-4">
-        <h1 className='font-inter font-semibold text-gray-700 text-base'> Nombre del Producto</h1>
-        <span className='font-inter text-sm text-gray-700 '> Categoría: </span>
-        <span className='font-inter font-semibold text-2xl'> $0.00 </span>
-        <p className='text-left max-w-md'> 
-            WeFind es una herramienta de busqueda de productos...
-        </p>
+    <Link href={`/product/${product.id}`}>
+      <div className="bg-white rounded-lg w-80 h-48 flex flex-row mt-2 p-4">
+        <Image
+          src={product.Image}
+          width={120}
+          height={30}
+          className="object-contain border-2"
+          alt={product.Name}
+        />
+        <div className="w-1/2 flex-col ml-4">
+          <h1 className="font-inter font-semibold text-gray-700 text-base">
+            {product.Name}
+          </h1>
+          <span className="font-inter font-semibold text-2xl">
+            RD${product.Price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </span>
+          <p className="text-left max-w-md">Distancia: {distance} km</p>
+        </div>
       </div>
+    </Link>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Card
+export default Card;
