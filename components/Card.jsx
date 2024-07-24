a1290acf97679efa118cc664d96c4ae065a512c0
Youssef <youssefcg1@gmail.com>
2024-07-24 18:50:38 -0400

    Modificacion de las cartas

--- components/Card.jsx
+++ components/Card.jsx
@@ -10,19 +10,29 @@ const Card = ({ product, distance }) => {
     window.location.href = `/detalles?id=${product.id}`;
   };
 
+  const truncateText = (text, limit) => {
+    if (text.length <= limit) return text;
+    return text.slice(0, limit) + '...';
+  };
+
+  const nameCharacterLimit = 30;
+
   return (
     <Link href={`/detalles?id=${product.id}`} onClick={handleClick}>
-      <div className="cursor-pointer bg-white rounded-lg w-80 h-48 flex flex-row p-4">
-        <Image
-          src={product.Image}
-          width={120}
-          height={30}
-          className="object-contain border-2"
-          alt={product.Name}
-        />
-        <div className="w-1/2 flex-col ml-4">
+      <div className="cursor-pointer bg-white rounded-lg w-80 h-48 flex flex-col items-center">
+        <div className='w-full h-3/5 overflow-hidden rounded-lg border-2'>
+          <Image
+            src={product.Image}
+            width={320}
+            height={100}
+            className='items-center object-cover w-full h-full'
+            alt={product.Name}
+          />
+        </div>
+        
+        <div className="w-full flex-col ml-4 pl-4">
           <h1 className="font-inter font-semibold text-gray-700 text-base">
-            {product.Name}
+            {truncateText(product.Name, nameCharacterLimit)}
           </h1>
           <span className="font-inter font-semibold text-2xl">
             RD${product.Price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
